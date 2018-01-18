162885bcd043fda8280f6f818aafb0f714956d38
Gábor Csuvik <gaborcsuvik@gmail.com>
2018-01-18 16:31:12 +0100

    WIP: Add Websocket to application

--- src/main/java/codes/gabor/datapipeline/messages/MessageController.java
+++ src/main/java/codes/gabor/datapipeline/messages/MessageController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -26,6 +28,8 @@ public class MessageController {
     }
 
     @PostMapping
+    @MessageMapping("/messages")
+    @SendTo("/live")
     public ResponseEntity createMessage(@RequestBody String message) {
         LOGGER.info("Request: POST:/api/messages - Add message to queue");
 
